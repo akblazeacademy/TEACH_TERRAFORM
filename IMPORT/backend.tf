66fba37733e48863636f7bd72d706ad2a37a3496
akblazeacademy <AKBLAZEACADEMY@GMAIL.COM>
2025-12-28 22:15:48 +0530

    Delete IMPORT directory

--- IMPORT/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "myprimary"
-    storage_account_name = "stgblaze"
-    container_name       = "statefile"
-    key                  = "import-lab.tfstate"
-  }
-}
-
